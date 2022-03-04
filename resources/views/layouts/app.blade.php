168ffcf896ef650a05a4cdc7d3bcc4eeebeffde1
Sina RashidiAzar <sinarashidiazar@hotmail.com>
2022-03-04 06:31:06 +0330

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Sina RashidiAzar</title>
+</head>
+<body>
+    <header>
+        {{-- Header Section --}}
+        @include('layouts.header')
+    </header>
+    <div>
+        {{-- Header Section --}}
+        @yield('content')
+    </div>
+    <footer>
+        {{-- Footer Section --}}
+        @include('layouts.footer')
+    </footer>
+</body>
+</html>
